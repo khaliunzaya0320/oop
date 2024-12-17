066a9254eda5bb4573fe804c6a511706626a420e
khaliunzaya tsogtbaatar <khaliunzayats@gmail.com>
2024-12-17 09:42:15 +0800

    test

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class List{
+
+  public:
+  // жагсаалтын сүүлд элемент нэмнэ
+  void add(T t){
+
+  }
+
+  //index - дүгээрт элеменет оруулна
+  void insert(T t, int index){
+
+  }
+
+  // i-р элементийн утгыг буцаана
+  T get(int index){
+
+  }
+
+  //i -р элементийг устгана
+  void delete(int index){
+
+  }
+
+  // //жагсаалтын уртыг буцаана
+  int length(){
+    
+  }
+};
+
+int main(){
+
+  return 0;
+}
